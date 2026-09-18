3a31cde2010665f58587d00afeabd9d58bad0b72
Tamzid <tamzidaljawad478@gmail.com>
2026-09-18 10:26:44 +0600

    Create 06_Bucket sort.cpp

---
+++ 02_Sorting and Searching Algorithms/06_Bucket sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bucketSort(vector<float>&arr, int n)
+{
+   
+    vector<float> buckets[n];
+  
+    for (int i = 0; i < n; i++)
+    {
+        int bucketIndex = n * arr[i];
+        buckets[bucketIndex].push_back(arr[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        sort(buckets[i].begin(), buckets[i].end());
+    }
+    int index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < buckets[i].size(); j++)
+        {
+            arr[index] = buckets[i][j];
+            index++;
+        }
+    }
+}
+
+int main()
+{
+    vector<float>arr{0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434};
+
+    bucketSort(arr, arr.size());
+
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
